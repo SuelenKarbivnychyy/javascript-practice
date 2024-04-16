5f5e8920ed8ce544c2aee2e425772fedc8e19e5c
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-04-15 21:17:32 -0400

    Developed logic for first function of objects.js

--- objects.js
+++ objects.js
@@ -2,8 +2,32 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
-}
+  // Return a dictionary of each word and the no. of times they appear.
+
+  //   You can assume that `phrase` does not contain any punctuation and that
+  //   each word is separated with a space.
+
+  //   Arguments:
+  //       phrase (str): A string of space-separated words
+
+  //   Returns:
+  //       dict: {word: # of times word appears in `phrase`}
+
+  let wordCounts = {};
+  
+  for ( const word of phrase.split(' ')) {
+    if (wordCounts.hasOwnProperty(word)) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+  
+  return wordCounts;
+
+};
+
+console.log(countWords("You with a space love You with a space kkk hhh"));
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
